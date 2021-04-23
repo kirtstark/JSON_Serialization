eefc14fc36a600ff3fac3106a516d47a5de6d6f4
Kirt Stark <kirtdstark@gmail.com>
2021-04-23 05:17:12 -0700

    Add files via upload
    This project is intended only as an example of serialization of JSON objects in a restful API and not as a complete restful API project.

---
+++ Controllers/DirectoryController.cs
@@ -0,0 +1,69 @@
+﻿using Directory.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Web.Http;
+
+namespace Directory.Controllers
+{
+    public class DirectoryController : ApiController
+    {
+        // GET: api/Directory
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Directory/5
+        public string Get(int id)
+        {
+            Rootobject ro = new Rootobject();
+
+            ro.id = 123456;
+            ro.firstName = "David";
+            ro.lastName = "Smith";
+            ro.age = 26;
+            ro.address = new Address();
+            ro.address.streetAddress = "1 A Street";
+            ro.address.city = "A Town";
+            ro.address.state = "WA";
+            ro.address.postalCode = "98625";
+            ro.phoneNumbers = new Phonenumber[2];
+            ro.phoneNumbers[0] = new Phonenumber();
+            ro.phoneNumbers[1] = new Phonenumber();
+            ro.phoneNumbers[0].number = "111.111.1111";
+            ro.phoneNumbers[0].type = "Cell";
+            ro.phoneNumbers[1].number = "111.111.1112";
+            ro.phoneNumbers[1].type = "Work";
+
+            string jsonString = JsonSerializer.Serialize(ro);
+            jsonString = JsonSerializer.Serialize(ro);
+
+            return jsonString;
+
+        }
+
+        // POST: api/Directory
+        public string Post([FromBody]Rootobject value)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                System.Web.HttpContextBase context = (System.Web.HttpContextBase)Request.Properties["MS_HttpContext"];
+                context.Request.InputStream.Seek(0, SeekOrigin.Begin);
+                context.Request.InputStream.CopyTo(stream);
+            }
+            
+            return value.firstName + " " + value.lastName + " " + (value.age != null ? value.age.ToString() : "");
+        }
+
+        // PUT: api/Directory/5
+        public void Put(int id, [FromBody]Rootobject value)
+        {
+        }
+
+        // DELETE: api/Directory/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Rootobject.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Directory.Models
+{
+    [DataContract]
+    public class Rootobject
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int id { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        [MaxLength(50)]
+        [Required]
+        public string firstName { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        [MaxLength(50)]
+        [Required]
+        public string lastName { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public int? age { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public Address address { get; set; }
+        public Phonenumber[] phoneNumbers { get; set; }
+    }
+
+    [DataContract]
+    public class Address
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string streetAddress { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string city { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string state { get; set;}
+        [DataMember(EmitDefaultValue = false)]
+        public string postalCode { get; set; }
+    }
+
+    [DataContract]
+    public class Phonenumber
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string type { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string number { get; set; }
+    }
+
+}
